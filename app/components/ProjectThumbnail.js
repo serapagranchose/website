0915bd81d897fb7d22d08730d82c5a9f0b6c9bf9
serapagranchose <seraphin.perrot@gmail.com>
2024-07-09 10:57:41 +0200

    Adding a way of fetching thumbnails without using gist's data

--- app/components/ProjectThumbnail.js
+++ app/components/ProjectThumbnail.js
@@ -1,14 +1,39 @@
 import Image from 'next/image';
+import { useState, useEffect } from 'react'
 
 export default function ProjectThumbnail({
   project
 }) {
+  const [thumbnailUrl, setThumbnailUrl] = useState("/no_thumbnail.gif");
+  useEffect(() => {
+    var gifRequest = new XMLHttpRequest();
+    var pngRequest = new XMLHttpRequest();
+
+    gifRequest.open("GET", `https://raw.githubusercontent.com/serapagranchose/${project?.name}/main/assets/images/thumbnail.gif`, true);
+    gifRequest.send();
+    gifRequest.onload = function() {
+      if (gifRequest.status == 200) {
+        setThumbnailUrl(`https://raw.githubusercontent.com/serapagranchose/${project?.name}/main/assets/images/thumbnail.gif`)
+      } else {
+        pngRequest.open("GET", `https://raw.githubusercontent.com/serapagranchose/${project?.name}/main/assets/images/thumbnail.png`, true);
+        pngRequest.send();
+        pngRequest.onload = function() {
+          if (pngRequest.status == 200) {
+            setThumbnailUrl(`https://raw.githubusercontent.com/serapagranchose/${project?.name}/main/assets/images/thumbnail.png`)
+          } else {
+            setThumbnailUrl("/no_thumbnail.gif")
+          }
+        }
+      }
+    }
+  });
+
   return (
     <>
       {project ?
         <Image
           className="pr-3"
-          src={project?.thumbnail_url ? project?.thumbnail_url : "/no_thumbnail.gif"}
+          src={thumbnailUrl}
           alt="thumbnail"
           width={2560}
           height={1440}
@@ -19,3 +44,4 @@ export default function ProjectThumbnail({
     </>
   );
 }
+
