d1087f1095921262cbb955e5287d7fa376d0d50c
serapagranchose <seraphin.perrot@gmail.com>
2024-09-05 15:03:50 +0200

    Fixing the responsive of the about page

--- app/about/page.js
+++ app/about/page.js
@@ -12,8 +12,8 @@ export default async function About() {
   return (
     <main className="flex flex-col h-screen">
       <Navbar />
-      <div className="p-2 pb-16 lg:pb-0 w-full flex-grow overflow-y-auto lg:grid lg:grid-cols-3 lg:grid-rows-3">
-        <div className="col-span-2 row-span-3 flex-grow overflow-y-auto scrollbar-hide scrollbar scrollbar-thumb-sky-700 scrollbar-track-transparent">
+      <div className="flex flex-col lg:flex-row p-2 pb-16 lg:pb-0 w-full flex-grow overflow-y-auto">
+        <div className="w-full lg:w-2/3 lg:flex-grow lg:overflow-y-auto lg:scrollbar-hide lg:scrollbar lg:scrollbar-thumb-sky-700 lg:scrollbar-track-transparent">
           <h2 className="text-2xl">WHO I AM</h2>
           <p>
             HI! I&#39;M SÉRAPHIN PERROT - FULL STACK DEVELOPER, PIXEL ARTIST.<br/>
@@ -51,20 +51,20 @@ export default async function About() {
             I STUDIED SPANISH FROM SECONDARY SCHOOL TO HIGH SCHOOL FOR 5 YEARS, I ESTIMATE MY LEVEL AT B1 BUT I INTEND TO IMPROVE IN THE FUTURE FOR THE ONE I LOVE.<br/>
           </p>
         </div>
-        <div className="col-span-1 mt-6 lg:mt-0 p-2 border border-4">
+        <div className="w-full lg:w-1/3 h-fit mt-6 lg:mt-0 p-2 border-4">
           <h2 className="text-2xl">CONFIGS</h2>
           <div className="flex justify-between">
             {data?.config.map((config, index) => {
               return (
-                <p key={index}>
-                  NAME: {config?.name.toUpperCase()}<br/>
-                  TYPE: {config?.type.toUpperCase()}<br/>
-                  OS: {config?.OS.toUpperCase()}<br/>
-                  DISK: {config?.Disk.toUpperCase()}<br/>
-                  MEM: {config?.Memory.toUpperCase()}<br/>
-                  CPU: {config?.CPU.toUpperCase()}<br/>
-                  GPU: {config?.GPU.toUpperCase()}<br/>
-                </p>
+                <div key={index} className="truncate">
+                  <p className="truncate">NAME: {config?.name.toUpperCase()}<br/></p>
+                  <p className="truncate">TYPE: {config?.type.toUpperCase()}<br/></p>
+                  <p className="truncate">OS: {config?.OS.toUpperCase()}<br/></p>
+                  <p className="truncate">DISK: {config?.Disk.toUpperCase()}<br/></p>
+                  <p className="truncate">MEM: {config?.Memory.toUpperCase()}<br/></p>
+                  <p className="truncate">CPU: {config?.CPU.toUpperCase()}<br/></p>
+                  <p className="truncate">GPU: {config?.GPU.toUpperCase()}<br/></p>
+                </div>
               )
             })}
           </div>
