f94dd707c8109d9eeab047e6223c62ee52947cbc
serapagranchose <seraphin.perrot@gmail.com>
2024-09-09 16:53:41 +0200

    Fixing google analytics

--- app/components/GoogleAnalytics.js
+++ app/components/GoogleAnalytics.js
@@ -4,21 +4,14 @@ import Script from 'next/script';
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script
-        strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-      />
+      <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}></Script>
+      <Script id='' strategy='lazyOnload'>{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-      <Script id='' strategy='lazyOnload'>
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
-          `}
-      </Script>
+        gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+      `}</Script>
     </>
   );
 };
